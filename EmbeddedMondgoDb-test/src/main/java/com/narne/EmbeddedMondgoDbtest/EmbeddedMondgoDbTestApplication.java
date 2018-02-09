6bb5ff100c9b0cd3e1c1bdad0b8e5bb56cdefdbd
Chas <Chas@chas.com>
2018-02-09 17:00:49 -0500

    Initial Commit EmbeddedMongoDb

---
+++ EmbeddedMondgoDb-test/src/main/java/com/narne/EmbeddedMondgoDbtest/EmbeddedMondgoDbTestApplication.java
@@ -0,0 +1,12 @@
+package com.narne.EmbeddedMondgoDbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedMondgoDbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedMondgoDbTestApplication.class, args);
+	}
+}
