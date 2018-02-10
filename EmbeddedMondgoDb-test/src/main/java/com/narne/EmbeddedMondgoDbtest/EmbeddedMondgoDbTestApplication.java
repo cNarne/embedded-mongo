41514c8aa492882f1912a2104632e8cf166db7e8
Chas <Chas@chas.com>
2018-02-10 15:48:18 -0500

    Master

--- EmbeddedMondgoDb-test/src/main/java/com/narne/EmbeddedMondgoDbtest/EmbeddedMondgoDbTestApplication.java
+++ EmbeddedMondgoDb-test/src/main/java/com/narne/EmbeddedMondgoDbtest/EmbeddedMondgoDbTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Adding comments
 // Adding more comments because
 // Adding even more comments.
+// Commit to master
 @SpringBootApplication
 public class EmbeddedMondgoDbTestApplication {
 
